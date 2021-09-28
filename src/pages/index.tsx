208bbf2e63fb0b3e308a018d2cdcd5e54e8b00e8
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-09-28 16:06:00 -0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Flex, Button, Stack} from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+
+        <Stack spacing="4">
+       
+       <Input name="email" type="email" label="E-mail"/>
+       <Input name="password" type="password" label="Password"/>
+
+      </Stack>
+
+       <Button type="submit" mt="6" colorScheme="pink" size="lg"> Entrar </Button>
+       
+      </Flex>
+    </Flex>
   )
 }
